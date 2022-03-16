8fef2c7102d81a6cb8a62999f9ea10d86eb605e7
RookieCoderrr <zilie.imtatlantique@gmail.com>
2022-03-16 12:02:16 +0800

    update service

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/robfig/cron/v3"
 	"log"
 	"net/http"
 )
@@ -13,28 +14,31 @@ import (
 
 func main()  {
 	fmt.Println("Server start")
-	//service.Sub(1644508800000,1644422400000)
-	tool.EncodeMd5("VZTxpIoXsrOKhFbF","nXlGyreGpwmmgclH","1644422400000")
+	//fmt.Println(time.Now().UnixNano()/ 1000000)
+	//fmt.Println( time.Now().UnixNano()/ 1000000 - 1647398951614)
+	//service.Sub(1647430804000,1647420004000)
+	tool.EncodeMd5("vnQiyDzZKufyyrQw","pPozWsLfNjQRQhnV","1647402231786")
 	cfg, err :=  tool.OpenConfigFile()
 	if err != nil {
 		log.Fatal(" open file error")
 	}
 	ctx := context.TODO()
-	co,_:=tool.IntializeMongoOnlineClient(cfg, ctx)
+	co,dbName:=tool.InitializeMongoOnlineClient(cfg, ctx)
 	s := &service.Service{
 		Db: co,
+		DbName: dbName,
 	}
 	muxRouter := mux.NewRouter()
 	muxRouter.HandleFunc("/projectId/{id}",s.AuthProjectId)
 	muxRouter.HandleFunc("/projectId/",s.ErrProjectId)
 	muxRouter.HandleFunc("/{params}",s.ErrProjectId)
 	muxRouter.HandleFunc("/",s.ErrProjectId)
-	//c := cron.New()
-	//c.AddFunc("@daily",func(){
-	//	fmt.Println("Start hourly job")
-	//	resetMethodCount()
-	//})
-	//c.Start()
+	c := cron.New()
+	c.AddFunc("@daily",func(){
+		fmt.Println("Start daily job")
+		tool.ResetRequestCount(co,context.TODO(),dbName)
+	})
+	c.Start()
 	http.ListenAndServe(":1926",muxRouter)
 
 }
