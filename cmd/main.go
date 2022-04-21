83e636ea3310b629a15c552c28773395cfb8473b
RookieCoderrr <zilie.imtatlantique@gmail.com>
2022-04-21 18:58:21 +0800

    runtime

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/robfig/cron/v3"
 	"log"
 	"net/http"
+	"os"
 )
 
 func init() {
@@ -20,6 +21,15 @@ func main()  {
 	//fmt.Println(time.Now().UnixNano()/ 1000000)
 	//fmt.Println( time.Now().UnixNano()/ 1000000 - 1647398951614)
 	//service.Sub(1647430804000,1647420004000)
+	rt := os.ExpandEnv("${RUNTIME}")
+	switch rt {
+	case "test":
+		fmt.Println("Runtime: test")
+	case "staging":
+		fmt.Println("Runtime: staging")
+	default:
+		fmt.Println("Runtime: default")
+	}
 	tool.EncodeMd5("465355e80ce88bbf542a58eee1dadedf","4ab781f72c0bdb4edfe1565282ff93e0","1648698459000")
 	cfg, err :=  tool.OpenConfigFile()
 	if err != nil {
